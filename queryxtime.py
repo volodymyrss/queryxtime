a3e57466b88b241a64d99d1678accea89d8e223d
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2019-05-02 13:01:23 +0000

    new format

--- queryxtime.py
+++ queryxtime.py
@@ -2,8 +2,10 @@ import requests
 import re
 
 xtime_url="https://heasarc.gsfc.nasa.gov/cgi-bin/Tools/xTime/xTime.pl"
-pattern="""<tr>.*?<th scope=row><label for="(.*?)">.*?</label></th>.*?<td align=center>.*?</td>.*?<td>(.*?)</td>.*?</tr>"""
-#pattern="""<tr>.*?<th scope=row><label for="(.*?)">(.*?)</label></th>.*?<td align=center>.*?</td>.*?<td>(.*?)</td>.*?</tr>"""
+pattern='<tr>(.*?)</tr>'
+#sub_pattern='.*?<th scope=row><label for="(.*?)">.*?</label></th>.*?<td align=center>.*?</td>.*?<td>(.*?)</td>.*?'
+sub_pattern='<th scope=row><label for="(.*?)">.*?</label></th>.*?<td align=center>.*?</td>.*?<td.*?>(.*?)</td>'
+#pattern='<tr>.*?<th scope=row><label for="(.*?)">.*?</label></th>.*?<td align=center>.*?</td>.*?<td>(.*?)</td>.*?</tr>'
 
 def queryxtime(**args):
     args=dict(args.items()+dict(
@@ -13,4 +15,15 @@ def queryxtime(**args):
 
     content=requests.get(xtime_url,params=args).content
 
-    return dict(re.findall(pattern,content,re.S))
+    #print("content",content)
+
+    r=[]
+    
+    for tr in re.findall(pattern,content,re.S):
+        print("tr",tr)
+        s = dict(re.findall(sub_pattern,tr,re.S))
+        print("s",s)
+
+        r+=list(s.items())
+
+    return dict(r)
